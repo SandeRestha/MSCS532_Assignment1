9c1ac94ab5e67f0e15ef769eabb86c606258a2de
SandeRestha <sthasandesh702@gmail.com>
2026-03-08 18:31:11 -0400

    Implemented insertion sort algorithm for decreasing order

--- insertion_sort_decreasing.py
+++ insertion_sort_decreasing.py
@@ -5,10 +5,28 @@ Insertion Sort (Monotonically Decreasing Order)
 
 def insertion_sort_decreasing(arr):
     """
-    Placeholder function for insertion sort.
-    Will be implemented in later commits.
+    Sorts an array in monotonically decreasing order
+    using the Insertion Sort algorithm.
     """
-    pass
+
+    # Traverse array starting from the second element
+    for i in range(1, len(arr)):
+
+        # Element to be inserted into sorted section
+        key = arr[i]
+
+        # Index of the previous element
+        j = i - 1
+
+        # Move smaller elements one position to the right
+        while j >= 0 and arr[j] < key:
+            arr[j + 1] = arr[j]
+            j -= 1
+
+        # Insert key at correct location
+        arr[j + 1] = key
+
+    return arr
 
 
 if __name__ == "__main__":
