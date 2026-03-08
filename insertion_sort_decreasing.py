854ed525270471d72ea5fae11c4fb8ca2f7219e1
SandeRestha <sthasandesh702@gmail.com>
2026-03-08 18:15:57 -0400

    first commit

---
+++ insertion_sort_decreasing.py
@@ -0,0 +1,16 @@
+"""
+MSCS532 - Assignment 1
+Insertion Sort (Monotonically Decreasing Order)
+"""
+
+def insertion_sort_decreasing(arr):
+    """
+    Placeholder function for insertion sort.
+    Will be implemented in later commits.
+    """
+    pass
+
+
+if __name__ == "__main__":
+    numbers = [31, 41, 59, 26, 41, 58]
+    print("Original Array:", numbers)
